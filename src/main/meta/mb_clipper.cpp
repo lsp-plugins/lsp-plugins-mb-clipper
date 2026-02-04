448d00df8fa33236ca44a8146e5f41e13dbacf00
Vladimir Sadovnikov <sadko4u@gmail.com>
2026-02-04 23:43:40 +0300

    Using new mechanism for registering plugin metadata

--- src/main/meta/mb_clipper.cpp
+++ src/main/meta/mb_clipper.cpp
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2025 Linux Studio Plugins Project <https://lsp-plug.in/>
- *           (C) 2025 Vladimir Sadovnikov <sadko4u@gmail.com>
+ * Copyright (C) 2026 Linux Studio Plugins Project <https://lsp-plug.in/>
+ *           (C) 2026 Vladimir Sadovnikov <sadko4u@gmail.com>
  *
  * This file is part of lsp-plugins-mb-clipper
  * Created on: 11 ноя 2023 г.
@@ -20,6 +20,7 @@
  */
 
 #include <lsp-plug.in/plug-fw/meta/ports.h>
+#include <lsp-plug.in/plug-fw/meta/registry.h>
 #include <lsp-plug.in/shared/meta/developers.h>
 #include <private/meta/mb_clipper.h>
 
@@ -344,6 +345,7 @@ namespace lsp
             mono_plugin_port_groups,
             &mb_clipper_bundle
         };
+        LSP_REGISTER_METADATA(mb_clipper_mono);
 
         const plugin_t mb_clipper_stereo =
         {
@@ -374,6 +376,8 @@ namespace lsp
             stereo_plugin_port_groups,
             &mb_clipper_bundle
         };
+        LSP_REGISTER_METADATA(mb_clipper_stereo);
+
     } /* namespace meta */
 } /* namespace lsp */
 
